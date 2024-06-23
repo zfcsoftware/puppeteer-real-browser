d4489acd6a40a40af885814d54d808aa3614aea2
Negezor <negezor@gmail.com>
2024-06-23 23:14:30 +1100

    refactor: simplify turnslite check stat

--- src/module/turnstile.js
+++ src/module/turnstile.js
@@ -1,40 +1,47 @@
-export const checkStat = ({ page }) => {
-    return new Promise(async (resolve, reject) => {
-        var st = setTimeout(() => {
-            clearInterval(st)
-            resolve(false)
-        }, 4000);
+export const checkStatNested = async ({ page }) => {
+    let hostname = '';
+
+    try {
+        const pageURL = await page.url();
+
+        const url = new URL(pageURL);
+
+        hostname = url.hostname;
+    } catch (err) { }
+
+    const frames = await page.frames().filter(frame => {
+        return frame.url().includes('cloudflare') || frame.url().includes(hostname)
+    });
+
+    if (frames.length <= 0) {
+        return false;
+    }
+
+    const elements = await page.$$('iframe');
+
+    for (const element of elements) {
         try {
-            var domain = '';
-            try {
-                const pageURL = await page.url();
-                const url = new URL(pageURL);
-                domain = url.hostname;
-            } catch (err) { }
-            const frames = await page.frames().filter(frame => {
-                return frame.url().includes('cloudflare') || frame.url().includes(domain)
-            });
-            if (frames.length <= 0) {
-                clearInterval(st)
-                return resolve(false)
+            const srcProperty = await element.getProperty('src');
+            const srcValue = await srcProperty.jsonValue();
+
+            if (srcValue.includes('turnstile')) {
+                await element.click();
             }
-            const elements = await page.$$('iframe');
-            for (const element of elements) {
-                try {
-                    const srcProperty = await element.getProperty('src');
-                    const srcValue = await srcProperty.jsonValue();
-                    if (srcValue.includes('turnstile')) {
-                        await element.click();
-                    }
-                } catch (err) { }
-            }
-            clearInterval(st)
-            resolve(true)
-        } catch (err) {
-            // console.log(err);
-            clearInterval(st)
-            resolve(false)
-        }
-    })
+        } catch (err) { }
+    }
+
+    return true;
 }
 
+export const checkStat = (params) => {
+    let interval;
+
+    return Promise.race([
+        new Promise((resolve) => {
+            interval = setTimeout(resolve, 4000, false);
+        }),
+        checkStatNested(params).catch((err) => false),
+    ]).finally(() => {
+        clearTimeout(interval);
+    });
+};
