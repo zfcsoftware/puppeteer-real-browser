2b366069846b18b8e0f852f056e0de9b8092f21d
rtritto <ariele.1994@gmail.com>
2024-08-04 17:47:25 +0200

    refactor: use async function instead of wrapping it in promise

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { sleep } from './module/general.js'
 import { checkStat } from './module/turnstile.js'
 import { protectPage } from 'puppeteer-afp'
 
-
 async function handleNewPage({ page, config = {} }) {
     // fp(page);
     protectPage(page, {
@@ -13,7 +12,7 @@ async function handleNewPage({ page, config = {} }) {
     return page
 }
 
-export const connect = ({
+export const connect = async ({
     args = [],
     headless = 'auto',
     customConfig = {},
@@ -24,144 +23,132 @@ export const connect = ({
     connectOption = {},
     fpconfig = {}
 }) => {
-    return new Promise(async (resolve, reject) => {
-        var global_target_status = false
+    var global_target_status = false
 
-        function targetFilter({ target, skipTarget }) {
-            if (target.type() === 'page') return true;
+    function targetFilter({ target, skipTarget }) {
+        if (target.type() === 'page') return true;
 
-            try { if (turnstile === true && target._getTargetInfo().type == "iframe") return false } catch (err) { }
+        try {
+            if (turnstile === true && target._getTargetInfo().type == "iframe") return false
+        } catch (err) { }
 
-            if (global_target_status === false) return true
+        if (global_target_status === false) return true
 
-            var response = false
-            try {
-                response = !!target.url()
-                if (skipTarget.find(item => String(target.url()).indexOf(String(item) > -1))) {
-                    response = true
-                }
-            } catch (err) { }
-            return response;
-        }
+        var response = false
 
-        const setTarget = ({ status = true }) => {
-            global_target_status = status
-        }
-
-        customConfig = {
-            ...customConfig,
-            ...connectOption,
-            targetFilter: (target) => targetFilter({ target: target, skipTarget: skipTarget }),
-        }
-        args.push('--no-sandbox', '--disable-setuid-sandbox', '--disable-blink-features=AutomationControlled', '--window-size=1920,1080');
-        const { browser, xvfbsession } = await startSession({
-            args: args,
-            headless: headless,
-            customConfig: customConfig,
-            proxy: proxy
-        })
-
-        var page = await browser.pages()
-
-        page = page[0]
-
-        setTarget({ status: true })
-
-        if (proxy && proxy.username && proxy.username.length > 0) {
-            await page.authenticate({ username: proxy.username, password: proxy.password });
-        }
-
-        var solve_status = true
-
-
-        const setSolveStatus = ({ status }) => {
-            solve_status = status
-        }
-
-        const autoSolve = ({ page }) => {
-            return new Promise(async (resolve, reject) => {
-                while (solve_status) {
-                    try {
-                        await sleep(1500)
-                        await checkStat({ page: page }).catch(err => { })
-                    } catch (err) { }
-                }
-                resolve()
-            })
-        }
-
-
-        if (fingerprint === true) {
-            handleNewPage({ page: page, config: fpconfig });
-        }
-        if (turnstile === true) {
-            setSolveStatus({ status: true })
-            autoSolve({ page: page, browser: browser })
-        }
-
-        // await page.setUserAgent(chromeSession.agent);
-
-        await page.setViewport({
-            width: 1920,
-            height: 1080
-        });
-
-        browser.on('disconnected', async () => {
-            // notice({
-            //     message: 'Browser Disconnected',
-            //     type: 'info'
-            // })
-            try { setSolveStatus({ status: false }) } catch (err) { }
-            await closeSession({
-                xvfbsession: xvfbsession
-            }).catch(err => { console.log(err.message); })
-        });
-
-
-        browser.on('targetcreated', async target => {
-            var newPage = await target.page();
-
-            try {
-                // await newPage.setUserAgent(chromeSession.agent);
-            } catch (err) {
-                // console.log(err.message);
+        try {
+            response = !!target.url()
+            if (skipTarget.find(item => String(target.url()).indexOf(String(item) > -1))) {
+                response = true
             }
+        } catch (err) { }
 
-            try {
-                await newPage.setViewport({
-                    width: 1920,
-                    height: 1080
-                });
-            } catch (err) {
-                // console.log(err.message);
-            }
+        return response;
+    }
 
+    const setTarget = ({ status = true }) => {
+        global_target_status = status
+    }
 
-
-
-
-
-            if (newPage && fingerprint === true) {
-                try {
-                    handleNewPage({ page: newPage, config: fpconfig });
-                } catch (err) { }
-            }
-
-            if (turnstile === true) {
-                autoSolve({ page: newPage })
-            }
-        });
-  
-        resolve({
-            browser: browser,
-            page: page,
-            xvfbsession: xvfbsession,
-            setTarget: setTarget
-        })
+    customConfig = {
+        ...customConfig,
+        ...connectOption,
+        targetFilter: (target) => targetFilter({ target: target, skipTarget: skipTarget }),
+    }
+    args.push('--no-sandbox', '--disable-setuid-sandbox', '--disable-blink-features=AutomationControlled', '--window-size=1920,1080');
+    const { browser, xvfbsession } = await startSession({
+        args: args,
+        headless: headless,
+        customConfig: customConfig,
+        proxy: proxy
     })
+
+    var page = await browser.pages()
+
+    page = page[0]
+
+    setTarget({ status: true })
+
+    if (proxy && proxy.username && proxy.username.length > 0) {
+        await page.authenticate({ username: proxy.username, password: proxy.password });
+    }
+
+    var solve_status = true
+
+    const setSolveStatus = ({ status }) => {
+        solve_status = status
+    }
+
+    const autoSolve = async ({ page }) => {
+        while (solve_status) {
+            try {
+                await sleep(1500)
+                await checkStat({ page: page }).catch(err => { })
+            } catch (err) { }
+        }
+    }
+
+    if (fingerprint === true) {
+        handleNewPage({ page: page, config: fpconfig });
+    }
+    if (turnstile === true) {
+        setSolveStatus({ status: true })
+        autoSolve({ page: page, browser: browser })
+    }
+
+    // await page.setUserAgent(chromeSession.agent);
+
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    });
+
+    browser.on('disconnected', async () => {
+        // notice({
+        //     message: 'Browser Disconnected',
+        //     type: 'info'
+        // })
+
+        try { setSolveStatus({ status: false }) } catch (err) { }
+
+        await closeSession({
+            xvfbsession: xvfbsession
+        }).catch(err => { console.log(err.message); })
+    });
+
+    browser.on('targetcreated', async target => {
+        var newPage = await target.page();
+
+        try {
+            // await newPage.setUserAgent(chromeSession.agent);
+        } catch (err) {
+            // console.log(err.message);
+        }
+
+        try {
+            await newPage.setViewport({
+                width: 1920,
+                height: 1080
+            });
+        } catch (err) {
+            // console.log(err.message);
+        }
+
+        if (newPage && fingerprint === true) {
+            try {
+                handleNewPage({ page: newPage, config: fpconfig });
+            } catch (err) { }
+        }
+
+        if (turnstile === true) {
+            autoSolve({ page: newPage })
+        }
+    });
+
+    return {
+        browser: browser,
+        page: page,
+        xvfbsession: xvfbsession,
+        setTarget: setTarget
+    }
 }
-
-
-
-
-
--- src/module/chromium.js
+++ src/module/chromium.js
@@ -15,82 +15,78 @@ export const closeSession = async ({ xvfbsession }) => {
     return true
 }
 
+export const startSession = async ({ args = [], headless = 'auto', customConfig = {}, proxy = {} }) => {
+    try {
+        var xvfbsession = null
+        var chromePath = customConfig.executablePath || customConfig.chromePath || puppeteer.executablePath()
 
-export const startSession = ({ args = [], headless = 'auto', customConfig = {}, proxy = {} }) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            var xvfbsession = null
-            var chromePath = customConfig.executablePath || customConfig.chromePath || puppeteer.executablePath()
+        const platform = slugify(process.platform)
 
-            const platform = slugify(process.platform)
-
-            if (platform.includes('linux') && headless === false) {
-                notice({
-                    message: 'This library is stable with headless: true in linuxt environment and headless: false in Windows environment. Please send headless: \'auto\' for the library to work efficiently.',
-                    type: 'error'
-                })
-            } else if (platform.includes('win') && headless === true) {
-                notice({
-                    message: 'This library is stable with headless: true in linuxt environment and headless: false in Windows environment. Please send headless: \'auto\' for the library to work efficiently.',
-                    type: 'error'
-                })
-            }
-
-            if (headless === 'auto') {
-                headless = platform.includes('linux') ? true : false
-            }
-
-            const chromeFlags = ['--no-sandbox', '--disable-setuid-sandbox', '--disable-blink-features=AutomationControlled', '--window-size=1920,1080'].concat(args);
-
-            if (headless === true) {
-                platform.includes('win') ? chromeFlags.push('--headless=new') : ''
-            }
-
-            if (proxy && proxy.host && proxy.host.length > 0) {
-                chromeFlags.push(`--proxy-server=${proxy.host}:${proxy.port}`);
-            }
-
-            if (process.platform === 'linux') {
-                try {
-                    const { default: Xvfb } = await import('xvfb')
-
-                    var xvfbsession = new Xvfb({
-                        silent: true,
-                        xvfb_args: ['-screen', '0', '1920x1080x24', '-ac']
-                    });
-                    xvfbsession.startSync();
-                } catch (err) {
-                    notice({
-                        message: `You are running on a Linux platform but do not have xvfb installed. The browser can be captured. Please install it with the following command\n\nsudo apt-get install xvfb\n\n${err.message}`,
-                        type: 'error'
-                    })
-                }
-            }
-            let dirPath = __dirname
-            if (!dirPath || dirPath.length === 0) {
-                const __filename = fileURLToPath(import.meta.url);
-                dirPath = dirname(__filename);
-            }
-            const EXTENSION_PATH = `${dirPath}/extension/`;
-            chromeFlags.push(`--disable-extensions-except=${EXTENSION_PATH}`)
-            chromeFlags.push(`--load-extension=${EXTENSION_PATH}`)
-
-            const browser = await puppeteer.launch({
-                headless: false, // Since it is in the testing phase, headless fixed is used and will be updated with the incoming value in the future.
-                executablePath: chromePath,
-                args: chromeFlags,
-                ...customConfig
+        if (platform.includes('linux') && headless === false) {
+            notice({
+                message: 'This library is stable with headless: true in linuxt environment and headless: false in Windows environment. Please send headless: \'auto\' for the library to work efficiently.',
+                type: 'error'
             })
-           
-            return resolve({
-                browser,
-                xvfbsession: xvfbsession
+        } else if (platform.includes('win') && headless === true) {
+            notice({
+                message: 'This library is stable with headless: true in linuxt environment and headless: false in Windows environment. Please send headless: \'auto\' for the library to work efficiently.',
+                type: 'error'
             })
-
-        } catch (err) {
-            console.log(err);
-            throw new Error(err.message)
         }
-    })
+
+        if (headless === 'auto') {
+            headless = platform.includes('linux') ? true : false
+        }
+
+        const chromeFlags = ['--no-sandbox', '--disable-setuid-sandbox', '--disable-blink-features=AutomationControlled', '--window-size=1920,1080'].concat(args);
+
+        if (headless === true) {
+            platform.includes('win') ? chromeFlags.push('--headless=new') : ''
+        }
+
+        if (proxy && proxy.host && proxy.host.length > 0) {
+            chromeFlags.push(`--proxy-server=${proxy.host}:${proxy.port}`);
+        }
+
+        if (process.platform === 'linux') {
+            try {
+                const { default: Xvfb } = await import('xvfb')
+
+                var xvfbsession = new Xvfb({
+                    silent: true,
+                    xvfb_args: ['-screen', '0', '1920x1080x24', '-ac']
+                });
+                xvfbsession.startSync();
+            } catch (err) {
+                notice({
+                    message: `You are running on a Linux platform but do not have xvfb installed. The browser can be captured. Please install it with the following command\n\nsudo apt-get install xvfb\n\n${err.message}`,
+                    type: 'error'
+                })
+            }
+        }
+        let dirPath = __dirname
+        if (!dirPath || dirPath.length === 0) {
+            const __filename = fileURLToPath(import.meta.url);
+            dirPath = dirname(__filename);
+        }
+        const EXTENSION_PATH = `${dirPath}/extension/`;
+        chromeFlags.push(`--disable-extensions-except=${EXTENSION_PATH}`)
+        chromeFlags.push(`--load-extension=${EXTENSION_PATH}`)
+
+        const browser = await puppeteer.launch({
+            headless: false, // Since it is in the testing phase, headless fixed is used and will be updated with the incoming value in the future.
+            executablePath: chromePath,
+            args: chromeFlags,
+            ...customConfig
+        })
+
+        return {
+            browser,
+            xvfbsession: xvfbsession
+        }
+    } catch (err) {
+        console.log(err);
+        throw new Error(err.message)
+    }
 }
 
